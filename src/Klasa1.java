3be5453b73853638e9e1d33f54e83af3ccc4c5d4
Malwina <malwinaleoszko@gmail.com>
2020-07-12 12:49:18 +0200

    Initial commit

---
+++ src/Klasa1.java
@@ -0,0 +1,11 @@
+public class Klasa1 {
+    public static void main(String[] args) {
+        System.out.println("Witaj w programie");
+        System.out.print("Witaj w programie");
+        String name="Malwina";
+        String surname="Leoszko";
+        System.out.println( name + "" + surname);
+        System.out.print("Koniec");
+
+    }
+}
